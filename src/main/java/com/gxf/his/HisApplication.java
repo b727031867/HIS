9522ba1b03cbf260f72d8bb452751d145581a565
gxf <727031867@qq.com>
2019-10-13 12:04:57 +0800

    初始化项目1.1

---
+++ src/main/java/com/gxf/his/HisApplication.java
@@ -0,0 +1,16 @@
+package com.gxf.his;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author GXF
+ * @date 2019-10-13 12:03:26
+ **/
+@SpringBootApplication
+public class HisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisApplication.class, args);
+	}
+
+}
